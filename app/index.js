c6d024d5149c5c579d64be61f24b98f3ffa40a44
Jim Newbery <james@tinnedfruit.com>
2017-03-28 18:12:01 +0100

    Quick and dirty choo version

--- app/index.js
+++ app/index.js
@@ -1,5 +1,32 @@
+import html from 'choo/html'
+import choo from 'choo'
+
+const app = choo()
+app.route('/', mainView)
+
+function mainView (state, emit) {
+  return html`
+    <div id="root" class="main">
+      <table class="c-MetaDataTable">
+        <thead>
+          <tr>
+            <th>Meta key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>twitter:card</td>
+            <td data-meta-value="twitter:card"></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  `
+}
+
 window.init = function (metaData) {
-  document.getElementById('root').innerHTML = 'WOOT';
+  app.mount('#root')
 }
 
 // function setBackground(backgroundPage) {
