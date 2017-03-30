1cb3e1ba1ae8b02cd2581e9fbe8556ad8477088d
Jim Newbery <james@tinnedfruit.com>
2017-03-30 18:32:30 +0100

    Tidy up

--- app/background/index.js
+++ app/background/index.js
@@ -3,12 +3,10 @@
 var metaDataCallback = null
 
 chrome.runtime.onMessage.addListener(function (details, sender, sendResponse) {
-  console.log('Received:', details)
   metaDataCallback(details)
 })
 
 window.getSocialCardMetaData = function (callback) {
-  console.log('Getting social card metadata')
   metaDataCallback = callback
   chrome.tabs.executeScript(null, {
     file: 'content.js'
