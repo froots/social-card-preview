bdb119781e4c31706f391bd3f3c2b08c51f8f300
Jim Newbery <james@tinnedfruit.com>
2017-03-31 09:57:30 +0100

    Refactor system test with page objects and cmds

---
+++ tests/system/page-objects/metaData.js
@@ -0,0 +1,22 @@
+var extensionCommands = require('./commands.js')
+
+var metaDataCommands = {
+  checkMetaDataTableValues: function (fixture, keys) {
+    keys.forEach(function (key) {
+      this.expect.element('[data-meta-value="' + key + '"]').text.to.equal(fixture[key])
+    }.bind(this))
+    return this
+  }
+}
+
+module.exports = {
+  url: function () {
+    return this.api.launchUrl
+  },
+  commands: [extensionCommands, metaDataCommands],
+  elements: {
+    loading: '.loading',
+    metaData: '.c-MetaData',
+    metaDataTable: '.c-MetaDataTable'
+  }
+}
